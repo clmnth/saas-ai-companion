074ece1eadb04fd069fde962dbbf53a0a9171914
Clmt <huvigclement@gmail.com>
2023-10-11 19:00:52 +0700

    category filter

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+// node script to insert the model category in the database 
+
+const { PrismaClient } = require("@prisma/client");
+
+const db  = new PrismaClient();
+
+async function main() {
+try {
+   await db.category.createMany({
+      data: [
+         { name: "Famous People" },
+         { name: "Movies & TV" },
+         { name: "Musicians" },
+         { name: "Games" },
+         { name: "Animals" },
+         { name: "Philosophy" },
+         { name: "Scientists" },
+      ]
+   })
+} catch (error) {
+   console.error("Error default categories", error);
+} finally {
+   await db.$disconnect();
+}
+};
+
+main();
