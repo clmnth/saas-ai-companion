3d6ac1a67606c63fdb087fa3d49ccf901efdb40a
Clmt <huvigclement@gmail.com>
2023-10-19 15:58:34 +0700

    chat ui done

--- app/(chat)/(routes)/chat/[chatId]/components/clients.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/clients.tsx
@@ -8,6 +8,7 @@ import { FormEvent, useState } from "react";
 import { ChatHeader } from "@/components/chat-header";
 import { ChatForm } from "@/components/chat -form";
 import { ChatMessages } from "@/components/chat-messages";
+import { ChatMessageProps } from "@/components/chat-message";
 
 interface ChatClientProps {
   companion: Companion & {
@@ -20,13 +21,13 @@ interface ChatClientProps {
 
 export const ChatClient = ({ companion }: ChatClientProps) => {
   const router = useRouter();
-  const [messages, setMessages] = useState<any[]>(companion.messages);
+  const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages);
 
   const { input, isLoading, handleInputChange, handleSubmit, setInput } =
     useCompletion({
       api: `/api/chat/${companion.id}`,
       onFinish(prompt, completion) {
-        const systemMessage = {
+        const systemMessage: ChatMessageProps = {
           role: "system",
           content: completion,
         };
@@ -38,7 +39,7 @@ export const ChatClient = ({ companion }: ChatClientProps) => {
     });
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    const userMessage = {
+    const userMessage: ChatMessageProps = {
       role: "user",
       content: input,
     };
